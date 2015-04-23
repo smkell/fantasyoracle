e7f6671e09e8424cb54fec18a79d2ad7ecd557e0
Sean Kell <sean.michael.kell@gmail.com>
2015-04-23 14:11:19 -0400

    Initial commit.

---
+++ CommandLineParser.Tests/CommandLineParserTests.fs
@@ -0,0 +1,78 @@
+﻿module CommandLineParser.Tests
+
+open NUnit.Framework
+
+[<Test>]
+let ``When I parse a command line with one argument then I should get a list with one token``() =
+    let argv = [ "test" ]
+    let expect = [ Argument "test" ]
+    let actual = parseCommandLine argv
+    Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I parse a command line with two arguments then I should get a list with two tokens``() =
+    let argv = [ "test"; "this" ]
+    let expect = [ Argument "test"; Argument "this" ]
+    let actual = parseCommandLine argv 
+    Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I parse a command line with a flag then I should get a list with one token``() =
+    for (argv, expect) in [ (["-d"], [BoolFlag "d"]); (["/d"], [BoolFlag "d"])] do
+        let actual = parseCommandLine argv
+        Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I parse a command line with a flag and an argument then I should get a list with two tokens``() =
+    let argv = ["test"; "-d"]
+    let expect = [Argument "test"; BoolFlag "d"]
+    let actual = parseCommandLine argv
+    Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I parse a command line with a value flag then I should get a list with one token``() =
+    for (argv, expect) in 
+        [ (["-o"; "test.txt"], [ValueFlag ("o", "test.txt")]);
+          (["-o=test.txt"], [ValueFlag ("o", "test.txt")]); 
+          (["/o:test.txt"], [ValueFlag ("o", "test.txt")])] do 
+        let actual = parseCommandLine argv
+        Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I have a command line with one argument, and an options record with one command then the command is initialized``() =
+    let argv = [ "query" ]
+
+    let executeQueryCommand tokens =
+        true
+    let query = { name="query"; cmd = executeQueryCommand}
+
+    let expect = { commands = [ query ]; options = []; arguments = [] }
+    let actual = parse [ query ] [] argv
+
+    Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I have a commandline specification with one command and one bool flag, and a command line with one argument then the command should be parsed``() =
+    let argv = [ "query"; ]
+
+    let executeQueryCommand tokens = 
+        true
+
+    let query = { name="query"; cmd = executeQueryCommand}
+    
+    let expect = { commands = [ query ]; options = [ ]; arguments = [] }
+    let actual = parse [ query ] [ BoolFlag "v" ] argv 
+    Assert.AreEqual(expect, actual)
+
+[<Test>]
+let ``When I have a commandline specificaiton with one command and no flags, and a command line with one command and a bool flag then the command should be parsed``() =
+    let argv = [ "query"; "-v" ]
+
+    let executeQueryCommand tokens =
+        true 
+
+    let query = { name="query"; cmd = executeQueryCommand}
+
+    let expect = { commands = [ query ]; options = [ ]; arguments = [] }
+    let actual = parse [ query ] [ ] argv 
+    Assert.AreEqual(expect, actual)
---
+++ FantasyOracle/Program.fs
@@ -0,0 +1,30 @@
+﻿open CommandLineParser 
+open FantasyOracle
+
+let executeQueryCommand commandline =
+    if (List.length commandline.arguments) < 1 then
+        printfn "ERROR: Not enough arguments for command 'query'"
+        false
+    else 
+        let players = commandline.arguments |> List.map Baseball.queryPlayersByName |> List.concat
+                
+        Baseball.printPlayerTableHeader
+        for player in players do
+            Baseball.printPlayerTableRow player
+        Baseball.printPlayerTableFooter players
+
+        true
+
+let query = { name = "query"; cmd = executeQueryCommand }
+
+let commands = [ query ]
+let options = [ ]
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let commandline = parse commands options (Array.toList argv)
+    
+    for cmd in commandline.commands do 
+        printfn "%b" (cmd.cmd commandline)
+    0 // return an integer exit code
